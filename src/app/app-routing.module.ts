315c24cc3dc23a7a31d1d8d3b721a2586d861c2a
Christa Stephens <christastephens@quickenloans.com>
2019-05-28 09:18:39 -0400

    creating routes for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+//where you define the routes of the page
+//within the array. Path is where they are
+//in the browser. We need to import the componenets
+//that we want to use for the routes
+const routes: Routes = [
+{path: '', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
