2529032a2158e8b60aca19e4f3f856bdeacafc69
Christa Stephens <christastephens@quickenloans.com>
2019-05-28 10:27:15 -0400

    fixed routing for nav bar links. Copied info and pasted it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-//where you define the routes of the page
-//within the array. Path is where they are
-//in the browser. We need to import the componenets
-//that we want to use for the routes
 const routes: Routes = [
-{path: '', component:HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
@@ -15,3 +15,12 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+//where you define the routes of the page
+//within the array. Path is where they are
+//in the browser. We need to import the componenets
+//that we want to use for the routes
+//'' gives url parameters, home is index
+//so it doesn't need to say 'Home'
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,8 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  firstClick() {
-    console.log('clicked');
+  firstClick(){
+    console.log("clicked");
   }
 
 }
