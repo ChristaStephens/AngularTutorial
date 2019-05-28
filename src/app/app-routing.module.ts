d4e3c40d1061418616de8e51938e0c0d06935ba8
Christa Stephens <christastephens@quickenloans.com>
2019-05-28 11:17:05 -0400

    fixed bug in app routing module ts, path has to be lower case to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
+  {path: "", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "contact", component: ContactComponent}
 ];
 
 @NgModule({
@@ -17,10 +17,10 @@ const routes: Routes = [
 export class AppRoutingModule { }
 
 
-
 //where you define the routes of the page
 //within the array. Path is where they are
 //in the browser. We need to import the componenets
 //that we want to use for the routes
 //'' gives url parameters, home is index
 //so it doesn't need to say 'Home'
+//path name has to match name in TS file (lowercase)
